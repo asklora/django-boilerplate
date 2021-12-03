74ce5a33546c1a7660ad6e7917950f9e5fc12a25
redloratech <rede.akbar@loratechai.com>
2021-12-03 13:57:51 +0700

    standart django template ORM ONLY

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,25 +20,12 @@ SECRET_KEY = 'django-insecure-d!8n=8bgj&qwuiv*4w8kvqi9u=ft3th2_y2#j(1(rm5)%3p##%
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'core.services',
     'core.utils',
-    'core.user'
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -51,9 +38,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+    
             ],
         },
     },
